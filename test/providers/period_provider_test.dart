aa37bbde790ae84498aa4ae2cc1116f7b47f3212
MiMo Code Agent <micode@example.com>
2026-07-02 09:26:01 +0800

    test: create test directory structure

---
+++ test/providers/period_provider_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('PeriodProvider', () {
+    // Tests will be added here
+  });
+}
---
+++ test/services/prediction_service_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('PredictionService', () {
+    // Tests will be added here
+  });
+}
